3b015bc3ae9ba7587ccd1f7b5eb44c0a109920b8
Matthew <26681440+mattyd96@users.noreply.github.com>
2022-02-23 13:29:59 +1030

    created prompt

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2'); //sql package for making sql queries
 const cTable = require('console.table'); // package for diaplaying database tables in console
 
 // Global Variables
-// Inquirer
+// Inquirer variables
 const questions = [
     {
         type: 'list',
@@ -12,3 +12,10 @@ const questions = [
         choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department']
     },
 ];
+
+// Inquirer Function
+const inquire = () => {
+    inquirer.prompt(questions).then((response) => {
+        //do something
+    });
+};
